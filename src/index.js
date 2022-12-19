6f5a9700f2973c93cbf626765b5a905cd7c07b01
Ben Cavins <ben.cavins@flatironschool.com>
2022-12-19 10:46:30 -0500

    add ramen menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', event => {
+    fetchRamen();
+});
+
+const menu = document.getElementById('ramen-menu');
+
+const form = document.getElementById('new-ramen');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+function fetchRamen() {
+    fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(ramens => {
+        ramens.forEach(ramen => {
+            const ramenImg = document.createElement('img');
+            ramenImg.src = ramen.image;
+            menu.appendChild(ramenImg);
+        });
+    });
+}
